5d90f6f24e5e6b9d2dfc29e0df097fe2846eaabb
Wolle <26044260+schreibfaul1@users.noreply.github.com>
2019-09-13 23:48:28 +0200

    Add files via upload

--- examples/CP_1251_cyrillic.ino
+++ examples/CP_1251_cyrillic.ino
@@ -11,28 +11,32 @@
 #include "SPI.h"
 #include "tft.h"
 
-#define TFT_CS        22    // do not use GPI032 or GPIO33 here
-#define TFT_DC        21    // do not use GPI032 or GPIO33 here
-#define SPI_MOSI      23
-#define SPI_MISO      19
-#define SPI_SCK       18
-#define TP_CS         16
+#define TFT_DC   21    /* Data or Command, do not use GPI032 or GPIO33 here*/
+#define TFT_CS   22    /* SPI Chip select, do not use GPI032 or GPIO33 here*/
+#define TFT_BL   17    /* BackLight */
+#define SPI_SCK  18
+#define SPI_MISO 19
+#define SPI_MOSI 23
+#define TFT_RST  16    /* Reset */
+#define TP_IRQ   39
+#define TP_CS    16
+#define SD_CS     5
 
 
-
-
-TFT tft(1);  //0=ILI9341, 1= HX8347D
+TFT tft(0);  //0=ILI9341, 1= HX8347D
 
 void setup() {
     String txt="";
     pinMode(TP_CS, OUTPUT);
     digitalWrite(TP_CS, HIGH); // disable the touchpad
+    pinMode(SD_CS, OUTPUT);
+    digitalWrite(SD_CS, HIGH); // disable SDcard
     SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
     tft.begin(TFT_CS, TFT_DC, SPI_MOSI, SPI_MISO, SPI_SCK);
     tft.fillScreen(TFT_BLACK);
     tft.setRotation(1);
     tft.setCursor(0, 0);
-    tft.setFont(Garamond23x24cyrillic);
+    tft.setFont(Garamond23x24cyrillic); // uncomment this font in tft,h
     tft.setTextColor(TFT_WHITE);
     txt="Парус – a poem by M.Lermontow\n";
     tft.println(txt);
@@ -47,6 +51,3 @@ void setup() {
 void loop(void) {
         delay(2000);
 }
-
-
-
