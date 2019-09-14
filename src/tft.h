a1876b7438461f72b2ed6dd666ee20c6c9bfb8f4
Wolle <26044260+schreibfaul1@users.noreply.github.com>
2019-09-14 15:53:55 +0200

    Update tft.h

--- src/tft.h
+++ src/tft.h
@@ -19,7 +19,7 @@ using namespace std;
 //#include "fonts/misc.h"                 // optional
 
 // this font needs a bigger partition change from "default" to "NO OTA (large app)"
-#include "fonts/Times_New_Roman.h"        // latin, greek, cyrillic with all extensions
+//#include "fonts/Times_New_Roman.h"      // latin, greek, cyrillic with all extensions
 
 
 
